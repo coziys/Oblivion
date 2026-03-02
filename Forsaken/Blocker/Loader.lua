32446368515a29472460ab6b459b68f46b1b13aa
coziys <satq.mc@gmail.com>
2026-03-02 20:23:54 +0900

    Update Loader.lua

--- Forsaken/Blocker/Loader.lua
+++ Forsaken/Blocker/Loader.lua
@@ -1,5 +1,9 @@
 if getconnections then
   --load v2
+  print("nice executor")
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/coziys/Oblivion/refs/heads/main/Forsaken/Blocker/v2.lua'))()
 else
   --load v3
+  print("wtf is that shitty executor")
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/coziys/Oblivion/refs/heads/main/Forsaken/Blocker/v3.lua'))()
 end
